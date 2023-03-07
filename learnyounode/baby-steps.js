54ae4244e42df29bc0a2b5c382bdf924b60c7123
hipolito082 <126525010+hipolito082@users.noreply.github.com>
2023-03-06 23:08:58 -0300

    Delete learnyounode directory

--- learnyounode/baby-steps.js
+++
@@ -1,8 +0,0 @@
-var soma = 0
-
-for (var i = 0; i < process.argv.length; i++) {
-    if (i >= 2) {
-        soma = soma + Number(process.argv[i])
-    }
-}
-console.log(soma)
--- learnyounode/file-server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  const fileStream = fs.createReadStream(process.argv[3]);
-  fileStream.pipe(res);
-});
-
-server.listen(process.argv[2]);
